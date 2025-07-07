642c2fbc0f0ea070d19c1ce807a776d908616703
yustyy <yusuf.acmaci@hotmail.com>
2025-07-08 02:13:53 +0300

    initial commit

---
+++ src/main/java/com/yusssss/mail_subscription/MailSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.yusssss.mail_subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailSubscriptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yusssss/mail_subscription/MailSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusssss.mail_subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
